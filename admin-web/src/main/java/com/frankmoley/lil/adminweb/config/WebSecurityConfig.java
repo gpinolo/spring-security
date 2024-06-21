d965bf69fe425914cbb478318cefdd6282fb285d
gdecesfc <giovanni.decesare1@walgreens.com>
2024-06-21 10:56:33 +0200

    In memory authentication (only for test environment

---
+++ admin-web/src/main/java/com/frankmoley/lil/adminweb/config/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.frankmoley.lil.adminweb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+/**
+ * In Spring boot 3 WebSecurityConfigurerAdapter is no longer available
+ * @see <a href="https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter">Spring Security without the WebSecurityConfigurerAdapter</a>
+ */
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig{
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/", "home").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User. withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+}
