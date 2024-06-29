6d1185b856d030fa87e5b04b96b6ede3ae90e66e
gpinolo <g.decesare@gmail.com>
2024-06-29 17:12:42 +0200

    Define UserDetailsService to avoid message "Using generated security password ..."

--- admin-web/src/main/java/com/frankmoley/lil/adminweb/config/WebSecurityConfig.java
+++ admin-web/src/main/java/com/frankmoley/lil/adminweb/config/WebSecurityConfig.java
@@ -3,10 +3,13 @@ package com.frankmoley.lil.adminweb.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.ldap.core.ContextSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.ldap.userdetails.LdapUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -35,6 +38,11 @@ public class WebSecurityConfig{
                         }
                 );
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(ContextSource context) {
+        return new LdapUserDetailsManager(context);
     }
 
     @Autowired
